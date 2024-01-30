ea6278055e2ee91f0dae5b71049ee482949adb4b
kkabir-b <kkabirbhalla@gmail.com>
2024-01-30 10:31:22 +0800

    Updated models.py

--- GISFirstDjango/models.py
+++ GISFirstDjango/models.py
@@ -4,6 +4,8 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
+    luck = models.IntegerField()
 
 class TeachingGroup(models.Model):
     students = models.OneToOneField(Person, null=True, on_delete=models.SET_NULL)
+
