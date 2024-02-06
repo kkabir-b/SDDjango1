d4c9df4bc01da04b98d59464c94f0a95893195a8
kkabir-b <kkabirbhalla@gmail.com>
2024-02-06 10:31:32 +0800

    Updated models.py

--- GISFirstDjango/views.py
+++ GISFirstDjango/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Person
 
 def splash(request):
-    name = '3'
+    name = '2'
     people = Person.objects.all()
     debug_people = list(people)
     return render(request, "splash.html", {'name': name})
