8136f66c864f5adfd55160042b1c99a41a7e5fab
kkabir-b <kkabirbhalla@gmail.com>
2024-02-20 10:21:42 +0800

    Updated models.py

--- GISFirstDjango/views.py
+++ GISFirstDjango/views.py
@@ -5,5 +5,5 @@ def splash(request):
     name = '2'
     people = Person.objects.all()
     debug_people = list(people)
-    lst = ['a','b','c']
+    lst = ['a','b','c','d']
     return render(request, "splash.html", {'lst': lst,'name':name})
